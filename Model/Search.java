bdfd085a399bc1f6d0655e6a5a01816ef2125443
TomAltankhuyag <60277488+TomAltankhuyag@users.noreply.github.com>
2021-12-09 00:02:44 -0700

    doSearch function implemented

--- Model/Search.java
+++ Model/Search.java
@@ -2,9 +2,10 @@ package Model;
 import java.util.*;
 public class Search  {
     private String type;
-    private int bed;
-    private int bath;
-    private boolean furn;
+    private double bed;
+    private double bath;
+    private int furn;
+    private int unfurn;
     private String quad;
     private ImportData datab;
     private List<Property> properties;
@@ -16,25 +17,49 @@ public class Search  {
         properties = datab.getProperties();
         criteria = datab.getSearches();
     }
-    public Search(String type, int bed, int bath, boolean furn, String quad) {
-        this.type = type;
-        this.bed = bed;
-        this.bath = bath;
-        this.furn = furn;
-        this.quad = quad;
-    }
 
+    /*
+     *  The function will iterate the properties and check for matching search criteria
+     *  @param idRenter  to check which search criteria the renter associates with
+     *  @return returns a list of property id strings that matches the search criteria
+     *          returns null otherwise
+     *
+     */
 
     public List<String> doSearch(String idRenter){
     updateLists();
         List<String> temp = new ArrayList<>();
+        if(!criteria.contains(idRenter)){
+            throw new RuntimeException("idRenter not found");
+        }
 
-        //for()
+        SearchCriteria search = criteria.get(criteria.indexOf(idRenter));
+
+        this.type = search.getApartmentType();
+        this.bed = search.getNoOfBedrooms();
+        this.bath = search.getNoOfBathrooms();
+        this.furn = search.getFurnished();
+        this.unfurn = search.getUnfurnished();
+        this.quad = search.getCityQuadrant();
+        for(Property p : properties){//iterate each property
+            //essentially if a criteria is not matching, we immediately move to the next iteration
+            // making sure the 'any' criteria is also not selected.
+            //if all the criteria matches, then we will add the property id to the temp list
+
+            if(!p.getApartmentType().equals(type)  && !type.equals("any"))continue;//make preference isn't 'any if it doesn't match
+            else if(p.getNoOfBedrooms() != bed && bed != 0)continue;
+            else if(p.getNoOfBathrooms() != bath && bath != 0)continue;
+            else if (p.getFurnished() != furn && (unfurn == 0))continue;//make sure both furn and unfurn aren't 1
+            else if(p.getUnfurnished() != unfurn && (furn == 0))continue;
+            else if (!p.getCityQuadrant().equals(quad) && !quad.equals(("any")))continue;//same idea as checking 'type'
+            else temp.add(p.getIdProperty());
+        }
+
+        if(temp.size() != 0) return temp;
+        else return null;
 
 
 
-        if(temp == null) return null;
-        else return temp;
 
     }
 
