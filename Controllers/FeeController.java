9d83ee21ae95bab7d6b3ae42e8e9b517ec6196d7
Haniya Ahmed <haniya.ahmed@ucalgary.ca>
2021-12-10 21:31:42 -0700

    documenting

--- Controllers/FeeController.java
+++ Controllers/FeeController.java
@@ -11,12 +11,27 @@ public class FeeController {
     ImportData model = new ImportData(dbsPath,
             dbsUser,dbsPass);
 
+    /**
+     *
+     * @param amount
+     */
     public void changeAmount (double amount){
         Update.setPropertyAmount(amount);
     }
+
+    /**
+     *
+     * @param period
+     */
     public void changePeriod (String period){
         Update.setPropertyAmount(period);
     }
+
+    /**
+     * sets amount and period
+     * @param amount
+     * @param period
+     */
     public void changeBoth (double amount, String period) {
         Update.setPropertyAmount(amount, period);
     }
